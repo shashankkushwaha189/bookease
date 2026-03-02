5e5222145aa42d61619aff4913faaa82fdfbe365
shashankkushwaha189 <shashankkushwaha189@gmail.com>
2026-03-02 19:22:42 +0530

    Fix Prisma client runtime dependencies for deployment

--- apps/api/dist/lib/prisma.js
+++ apps/api/dist/lib/prisma.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const client_1 = require("@prisma/client");
+const client_1 = require("../generated/client");
 const pg_1 = require("pg");
 const env_1 = require("../config/env");
 const globalForPrisma = global;
